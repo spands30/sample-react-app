6864ebb21cfe58109e4cc8b34d5c85e5ec767949
spandana <spands30@gmail.com>
2018-04-26 06:44:06 +0530

    Issue Fixed on webpack
    Fixing the webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ entry:'./src/js/index.js',
   output:{
     filename: 'bundle.js'
   },
-}
+
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   }
+}
